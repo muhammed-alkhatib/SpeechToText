d893b878b49564d44122631daf6c9265f36ed2f7
mohammed <mohammed12alkhatib@gmail.com>
2025-10-30 18:45:14 +0300

    Add project files.

---
+++ MainForm.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace SpeechToTextVoskApp
+{
+    partial class MainForm
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Label lblStatus;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.btnStart.Location = new System.Drawing.Point(12, 12);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(160, 40);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Kaydı Başlat";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Font = new System.Drawing.Font("Segoe UI", 11F);
+            this.txtOutput.Location = new System.Drawing.Point(12, 70);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtOutput.Size = new System.Drawing.Size(560, 280);
+            this.txtOutput.TabIndex = 1;
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.lblStatus.Location = new System.Drawing.Point(190, 22);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(107, 19);
+            this.lblStatus.TabIndex = 2;
+            this.lblStatus.Text = "Durum: Bekliyor";
+            // 
+            // MainForm
+            // 
+            this.ClientSize = new System.Drawing.Size(584, 361);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.btnStart);
+            this.Name = "MainForm";
+            this.Text = "Speech To Text (Vosk)";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+    }
+}
---
+++ MainForm.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NAudio.Wave;
+using Vosk;
+using Newtonsoft.Json.Linq;
+
+namespace SpeechToTextVoskApp
+{
+    public partial class MainForm : Form
+    {
+        private WaveInEvent waveIn;
+        private VoskRecognizer recognizer;
+        private Model model;
+        private bool isRecording = false;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnStart_Click(object sender, EventArgs e)
+        {
+            if (!isRecording)
+            {
+                await StartRecordingAsync();
+            }
+            else
+            {
+                StopRecording();
+            }
+        }
+
+        private async Task StartRecordingAsync()
+        {
+            try
+            {
+                lblStatus.Text = "Durum: Model yükleniyor...";
+                await Task.Run(() => model = new Model("model")); // ضع هنا مسار مجلد النموذج
+                recognizer = new VoskRecognizer(model, 16000.0f);
+
+                waveIn = new WaveInEvent
+                {
+                    DeviceNumber = 0,
+                    WaveFormat = new WaveFormat(16000, 1)
+                };
+
+                waveIn.DataAvailable += (s, a) =>
+                {
+                    if (recognizer.AcceptWaveform(a.Buffer, a.BytesRecorded))
+                    {
+                        var text = JObject.Parse(recognizer.Result())["text"]?.ToString();
+                        if (!string.IsNullOrWhiteSpace(text))
+                            AppendTextSafe("👉 " + text + Environment.NewLine);
+                    }
+                    else
+                    {
+                        var partial = JObject.Parse(recognizer.PartialResult())["partial"]?.ToString();
+                        if (!string.IsNullOrWhiteSpace(partial))
+                            AppendTextSafe("⏳ " + partial + Environment.NewLine);
+                    }
+                };
+
+                waveIn.StartRecording();
+                lblStatus.Text = "Durum: Dinliyor 🎤";
+                btnStart.Text = "Durdur";
+                isRecording = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Hata: " + ex.Message);
+            }
+        }
+
+        private void StopRecording()
+        {
+            waveIn?.StopRecording();
+            waveIn?.Dispose();
+            recognizer?.Dispose();
+            model?.Dispose();
+
+            lblStatus.Text = "Durum: Durduruldu ⏹️";
+            btnStart.Text = "Kaydı Başlat";
+            isRecording = false;
+        }
+
+        private void AppendTextSafe(string text)
+        {
+            if (txtOutput.InvokeRequired)
+                txtOutput.Invoke(new Action(() => txtOutput.AppendText(text)));
+            else
+                txtOutput.AppendText(text);
+        }
+    }
+}
